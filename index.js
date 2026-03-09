16a1604a5348e951441a5b651463a326b67e4537
1Dkvr <1D.KVR.user@gmail.com>
2026-03-09 13:29:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,61 +10,73 @@ import { Ø1D } from "./Humans.js";
  * @updated 2026-03-09
  */
 export class uRTC {
-    constructor(roomId = "uRTC-default-room") {
+    constructor(roomId = "default-room") {
         this.roomId = roomId;
-        this.peers = new Map();
+        this.connections = {};
         
-        // Callbacks
         this.onMessage = (from, msg) => {};
         this.onPeerConnect = (id) => {};
         
-        this._loadDependencies().then(() => this._init());
+        this._init();
     }
 
-    async _loadDependencies() {
-        if (window.WebTorrent) return;
-        return new Promise((resolve) => {
-            const script = document.createElement('script');
-            script.src = "https://cdn.jsdelivr.net/npm/webtorrent@latest/webtorrent.min.js";
-            script.onload = resolve;
-            document.head.appendChild(script);
-        });
-    }
-
-    _init() {
-        // @ts-ignore
-        this.client = new WebTorrent();
-        const infoHash = this._generateHash(this.roomId);
-
-        // On utilise un torrent vide comme bus de communication
-        this.client.seed(new Uint8Array(0), { name: "uRTC-Bus", infoHash }, (torrent) => {
-            console.log("uRTC: Room rejointe", torrent.infoHash);
-
-            torrent.on('wire', (wire) => {
-                const peerId = wire.remoteAddress || Math.random().toString(36).slice(2);
-                if (!this.peers.has(peerId)) {
-                    this.peers.set(peerId, wire);
-                    this.onPeerConnect(peerId);
-                    
-                    // Écoute des messages
-                    wire.on('extended', (id, data) => {
-                        if (id === 'uRTC-msg') this.onMessage(peerId, data.toString());
-                    });
-                }
+    async _init() {
+        // Chargement dynamique de la lib si pas présente
+        if (!window.Peer) {
+            await new Promise(r => {
+                const s = document.createElement('script');
+                s.src = "https://unpkg.com/peerjs@1.5.2/dist/peerjs.min.js";
+                s.onload = r;
+                document.head.appendChild(s);
             });
+        }
+
+        // On utilise un ID basé sur la room + un random pour être unique
+        this.myId = this.roomId + "-" + Math.random().toString(36).slice(2, 6);
+        
+        // Connexion au serveur de signalisation public de PeerJS
+        this.peer = new Peer(this.myId);
+
+        this.peer.on('open', (id) => {
+            console.log("uRTC: Connecté au réseau sous l'ID", id);
+            this._discoverPeers();
+        });
+
+        // Écoute les appels entrants
+        this.peer.on('connection', (conn) => {
+            this._setupConnection(conn);
+        });
+
+        this.peer.on('error', (err) => console.error("uRTC Peer Error:", err));
+    }
+
+    _discoverPeers() {
+        // Dans un système sans liste de serveurs, on ne peut pas "deviner" les autres.
+        // On demande à l'utilisateur de partager son ID ou on utilise un système de 'Room'
+        // Pour l'instant, on simule la découverte :
+        console.warn("Partagez cet ID pour que d'autres rejoignent :", this.myId);
+    }
+
+    connect(remoteId) {
+        if (this.connections[remoteId]) return;
+        const conn = this.peer.connect(remoteId);
+        this._setupConnection(conn);
+    }
+
+    _setupConnection(conn) {
+        this.connections[conn.peer] = conn;
+        conn.on('open', () => {
+            this.onPeerConnect(conn.peer);
+            conn.send({type: "system", msg: "Hello!"});
+        });
+        conn.on('data', (data) => {
+            this.onMessage(conn.peer, data);
         });
     }
 
     send(msg) {
-        this.peers.forEach(wire => {
-            try { wire.extended('uRTC-msg', msg); } catch(e) {}
+        Object.values(this.connections).forEach(conn => {
+            if (conn.open) conn.send(msg);
         });
-    }
-
-    _generateHash(str) {
-        // Simple hash pour transformer le nom de la room en InfoHash BitTorrent (40 hex chars)
-        let hash = "";
-        for (let i = 0; i < 40; i++) hash += (str.charCodeAt(i % str.length) % 16).toString(16);
-        return hash;
     }
 }
