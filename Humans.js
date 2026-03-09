bf7a7d39358f7e1fcb455b640811e97955fd936b
1Dkvr <1D.KVR.user@gmail.com>
2026-03-09 01:17:30 +0100

    Dynamic identity resolver
    This file implements a dynamic identity resolver that connects global identity with the local project context.

---
+++ Humans.js
@@ -0,0 +1,7 @@
+/**
+ * @file Humans.js
+ * @description Dynamic identity resolver. Bridges global identity with local project context.
+ */
+import { Ø1D } from "https://1dkvr.github.io/FrameKit/Human.js";
+Ø1D.attach("uRTC");
+export { Ø1D };
