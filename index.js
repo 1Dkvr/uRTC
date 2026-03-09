9d2a2c2d38bf644d3de36bd5ae8daea657bf7d8d
1Dkvr <1D.KVR.user@gmail.com>
2026-03-09 13:27:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,126 +3,68 @@ import { Ø1D } from "./Humans.js";
 /**
  * @class uRTC
  * @description An ultra-performant, zero-dependency WebRTC wrapper for P2P data & file synchronization. Multi-peer WebRTC wrapper for high-speed P2P sync and media.
- * @version 1.0.1316
+ * @version 1.0.1327
  * @author 1D
  * @copyright © 2026 Hold'inCorp. All rights reserved.
  * @license Apache-2.0
  * @updated 2026-03-09
  */
 export class uRTC {
-    constructor(roomId) {
+    constructor(roomId = "uRTC-default-room") {
         this.roomId = roomId;
-        this.myId = Math.random().toString(36).substring(7);
-        this.peers = {}; // Stocke les RTCPeerConnection
-        this.localStream = null;
-
-        // Callbacks publics
+        this.peers = new Map();
+        
+        // Callbacks
         this.onMessage = (from, msg) => {};
-        this.onPeerCount = (count) => {};
-        this.onStream = (id, stream) => {};
-
-        this._initFirebase();
+        this.onPeerConnect = (id) => {};
+        
+        this._loadDependencies().then(() => this._init());
     }
 
-    _initFirebase() {
-        // Configuration publique de test (Google Firebase)
-        // Note: Pour une prod réelle, tu créeras ton propre projet en 2min.
-        const config = {
-            databaseURL: "https://urtc-p2p-default-rtdb.europe-west1.firebasedatabase.app"
-        };
-        
+    async _loadDependencies() {
+        if (window.WebTorrent) return;
+        return new Promise((resolve) => {
+            const script = document.createElement('script');
+            script.src = "https://cdn.jsdelivr.net/npm/webtorrent@latest/webtorrent.min.js";
+            script.onload = resolve;
+            document.head.appendChild(script);
+        });
+    }
+
+    _init() {
         // @ts-ignore
-        firebase.initializeApp(config);
-        this.db = firebase.database().ref(`rooms/${this.roomId}`);
+        this.client = new WebTorrent();
+        const infoHash = this._generateHash(this.roomId);
 
-        // 1. J'annonce ma présence
-        const myRef = this.db.child('peers').child(this.myId);
-        myRef.set({ id: this.myId, joinedAt: firebase.database.ServerValue.TIMESTAMP });
-        myRef.onDisconnect().remove();
+        // On utilise un torrent vide comme bus de communication
+        this.client.seed(new Uint8Array(0), { name: "uRTC-Bus", infoHash }, (torrent) => {
+            console.log("uRTC: Room rejointe", torrent.infoHash);
 
-        // 2. J'écoute les autres arrivants
-        this.db.child('peers').on('value', snapshot => {
-            const peers = snapshot.val() || {};
-            const count = Object.keys(peers).length;
-            this.onPeerCount(count);
-            
-            Object.keys(peers).forEach(peerId => {
-                if (peerId !== this.myId && !this.peers[peerId]) {
-                    this._initConnection(peerId, true);
+            torrent.on('wire', (wire) => {
+                const peerId = wire.remoteAddress || Math.random().toString(36).slice(2);
+                if (!this.peers.has(peerId)) {
+                    this.peers.set(peerId, wire);
+                    this.onPeerConnect(peerId);
+                    
+                    // Écoute des messages
+                    wire.on('extended', (id, data) => {
+                        if (id === 'uRTC-msg') this.onMessage(peerId, data.toString());
+                    });
                 }
             });
         });
-
-        // 3. J'écoute les signaux WebRTC (SDP/ICE)
-        this.db.child('signals').child(this.myId).on('child_added', snapshot => {
-            const data = snapshot.val();
-            this._handleSignal(data.from, data.signal);
-            snapshot.ref.remove(); // Nettoyage immédiat
-        });
-    }
-
-    _initConnection(peerId, isOfferer) {
-        const pc = new RTCPeerConnection({
-            iceServers: [{ urls: "stun:stun.l.google.com:19302" }]
-        });
-        this.peers[peerId] = pc;
-
-        // Data Channel pour le chat
-        const dc = isOfferer ? pc.createDataChannel("chat") : null;
-        if (dc) this._setupData(peerId, dc);
-
-        pc.ondatachannel = e => this._setupData(peerId, e.channel);
-
-        pc.onicecandidate = e => {
-            if (e.candidate) this._sendSignal(peerId, { candidate: e.candidate });
-        };
-
-        pc.ontrack = e => this.onStream(peerId, e.streams[0]);
-
-        if (isOfferer) {
-            pc.onnegotiationneeded = async () => {
-                const offer = await pc.createOffer();
-                await pc.setLocalDescription(offer);
-                this._sendSignal(peerId, { sdp: pc.localDescription });
-            };
-        }
-    }
-
-    async _handleSignal(from, signal) {
-        if (!this.peers[from]) this._initConnection(from, false);
-        const pc = this.peers[from];
-
-        if (signal.sdp) {
-            await pc.setRemoteDescription(new RTCSessionDescription(signal.sdp));
-            if (signal.sdp.type === 'offer') {
-                const answer = await pc.createAnswer();
-                await pc.setLocalDescription(answer);
-                this._sendSignal(from, { sdp: pc.localDescription });
-            }
-        } else if (signal.candidate) {
-            await pc.addIceCandidate(new RTCIceCandidate(signal.candidate));
-        }
-    }
-
-    _setupData(peerId, dc) {
-        dc.onmessage = e => this.onMessage(peerId, e.data);
-    }
-
-    _sendSignal(to, signal) {
-        this.db.child('signals').child(to).push({ from: this.myId, signal });
     }
 
     send(msg) {
-        // Envoi via tous les DataChannels ouverts (Mesh)
-        Object.values(this.peers).forEach(pc => {
-            // Logique simplifiée : on récupère le canal ouvert
+        this.peers.forEach(wire => {
+            try { wire.extended('uRTC-msg', msg); } catch(e) {}
         });
-        // Pour le test, on peut aussi repasser par la DB pour le chat broadcast
-        this.db.child('chat').push({ from: this.myId, msg, time: Date.now() });
     }
 
-    async enableMedia() {
-        this.localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:true});
-        return this.localStream;
+    _generateHash(str) {
+        // Simple hash pour transformer le nom de la room en InfoHash BitTorrent (40 hex chars)
+        let hash = "";
+        for (let i = 0; i < 40; i++) hash += (str.charCodeAt(i % str.length) % 16).toString(16);
+        return hash;
     }
 }
