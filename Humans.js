6259cd295588fe96f971f246966cec9a54cc10a9
1Dkvr <1D.KVR.user@gmail.com>
2026-03-13 11:10:10 +0100

    Local identity bridge

--- Humans.js
+++ Humans.js
@@ -1,7 +1,12 @@
 /**
  * @file Humans.js
- * @description Dynamic identity resolver. Bridges global identity with local project context.
+ * @description Local identity bridge.
+ * @version 1.0.0000
+ * @author 1D
+ * @updated 2026.03.13
+ * @copyright © 2026 Hold'inCorp. All rights reserved.
+ * @see https://1dkvr.github.io/FrameKit/Human.js
  */
-import { Ø1D } from "https://1dkvr.github.io/FrameKit/Human.js";
+import Ø1D from "https://1dkvr.github.io/FrameKit/Human.js";
 Ø1D.attach("uRTC");
-export { Ø1D };
+export { Ø1D as default, Ø1D };
